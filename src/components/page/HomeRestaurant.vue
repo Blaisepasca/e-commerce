54b7acefaa195e534d7a3e7e431f9106689dca7e
Blaise <blaiseaganzepascal10@gmail.com>
2024-03-22 09:17:03 -0700

    initial

--- src/components/page/HomeRestaurant.vue
+++ src/components/page/HomeRestaurant.vue
@@ -2,7 +2,12 @@
   <div class="home">
     <div class="header">
       <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="">
-      <input v-model="user_search_restaurant" type="text" placeholder="What Do You Need">
+      <div class="wripprer--input">
+        <input v-model="user_search_restaurant" type="text" placeholder="What Do You Need">
+      </div>
+      <div class="search"></div>
+
+     
      
     </div>
     <div class="bannier"></div>
@@ -56,9 +61,14 @@ export default {
     // user search restaurant
 
     let user_search_restaurant = ref('');
+    let search_restaurant =ref([])
 
-    watch(user_search_restaurant, (new_value)=>{
-      console.log(new_value,all_restaurant);
+    watch(user_search_restaurant, new_value=> {
+      let regex = RegExp(new_value);
+
+      let new_search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name));
+      search_restaurant = new_search_restaurant
+      
 
     })
 
@@ -69,6 +79,7 @@ export default {
     return{
       date_restaurant,
       user_search_restaurant,
+      search_restaurant,
     }
     
   },
@@ -86,22 +97,34 @@ export default {
     img{
       width:200px;
     }
-    .input{
+    .wripprer--input{
+      position: relative;
+      .input{
       background-color: #f6f6f6;
       height:100px ;
       width:50px ;
       border: none;
       outline: none;
       padding-left: 20px;
-      
-      
+       }
+       .search{
+        position: absolute;
+        top: 10%;
+        width: 50%;
+        height: 400px;
+        background-color: #ffffff;
+        border: solid 1px red;
+        background-color: red;
+       }
     }
+  
     .bannier{
       height:200px ;
       width: 100px;
-      background-image: '';
+      background-image: 'https://stock.adobe.com/search?k=food+delivery&asset_id=301327108';
       background-size: cover;
       background-position: bottom ;
+      background-color: red;
     }
   }
 }
